ad42d2caeadbfac4b039b65aad78682c6978a486
aarya <chat.the.gpt65@gmail.com>
2026-02-27 18:06:29 +0300

    operations

---
+++ Lesson 6/AND.py
@@ -0,0 +1,20 @@
+a=5
+b=6
+c=7
+if a and b and c:
+    print("All the number as true")
+else:
+    print("At least one number has boolean value as false")
+
+a=3
+b=-8
+c=0
+if a>0 or b>0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("No number is greater than 0")
+
+if b>0 or c>0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("No number is greater than 0")
