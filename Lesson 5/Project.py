617d4a5791b9353c92dffe8a0149925b8513a174
aarya <chat.the.gpt65@gmail.com>
2026-03-08 12:20:16 +0300

    Temperature

---
+++ Lesson 5/Project.py
@@ -0,0 +1,7 @@
+temp= float(input("Enter current temperature in Celsius: "))
+if temp>25:
+    print("It is considerably hot now. You can wear light clothes!")
+elif temp>20:
+    print("Not much use of a jacket, but a sweater is preferable.")
+else:
+    print("It is cold outside. Stick to your jackets and pullovers.")
