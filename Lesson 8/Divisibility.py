d5ac3afd992f6e3c676508195f1a3f7053859f59
aarya <chat.the.gpt65@gmail.com>
2026-03-12 14:06:07 +0300

    divide

---
+++ Lesson 8/Divisibility.py
@@ -0,0 +1,6 @@
+n1=int(input("Enter divident: "))
+n2=int(input("Enter divisor: "))
+if n1%n2==0:
+    print(str(n1),"is divisible by",str(n2))
+else:
+    print(str(n1),"is not divisible by",str(n2))
