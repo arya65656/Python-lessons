71acbda57afe20d69a19c5ec2e0b5fcda7800738
aarya <chat.the.gpt65@gmail.com>
2026-03-12 19:11:21 +0300

    Age

---
+++ Lesson 9/Enrollment.py
@@ -0,0 +1,5 @@
+age = int(input("Enter the student's age: "))
+if age >= 10 and age <= 20:
+    print("Enrollment successful! The student is allowed.")
+else:
+    print("Enrollment failed. Students must be between 10 and 20 years old.")
