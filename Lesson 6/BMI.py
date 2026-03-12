073ebcca38fa02f76d6586ea85ff7361e3ed4f93
aarya <chat.the.gpt65@gmail.com>
2026-03-12 13:54:05 +0300

    order

--- Lesson 6/BMI.py
+++ Lesson 6/BMI.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 h=float(input("Enter height in meters:"))
 w=float(input("Enter weight in kilograms:"))
 bmi=w/(h**2)
@@ -15,21 +14,4 @@ elif bmi<=39.9:
     print("This person is obese")
 else:
     print("This person is severly obese")
-=======
-h=float(input("Enter height in meters:"))
-w=float(input("Enter weight in kilograms:"))
-bmi=w/(h**2)
-print("Your BMI is ", bmi)
-if bmi<=18.4:
-    print("This person is underweight")
-elif bmi<=24.9:
-    print("This person is healthy")
-elif bmi<=29.9:
-    print("This person is overweight")
-elif bmi<=34.9:
-    print("This person is severly overweight")
-elif bmi<=39.9:
-    print("This person is obese")
-else:
-    print("This person is severly obese")
->>>>>>> 0ab3677f37dce7007943f02132a0929b1537d287
+    
---
+++ Lesson 8/project.py
@@ -0,0 +1,14 @@
+z=20
+x=30
+c=90
+
+v=z*(x/c)
+print(z, x, c,"\n", v)
+
+temp=c
+c=x
+x=z
+z=temp
+
+v=z*(x/c)
+print(z, x, c,"\n", v)
