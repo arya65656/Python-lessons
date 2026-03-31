95b7b7cbe524b63c33072b2f7653a8b1ff3077d0
aarya <chat.the.gpt65@gmail.com>
2026-03-31 10:20:43 +0300

    square

--- Lesson 14/sqr.py
+++ Lesson 14/sqr.py
@@ -1,6 +1,10 @@
 import turtle
-pen = turtle.Turtle()
+pen=turtle.Turtle()
+pen.color("blue")
+pen.fillcolor("yellow")
+pen.begin_fill()
 for i in range(4):
     pen.forward(100)
     pen.right(90)
+pen.end_fill()
 turtle.done()
