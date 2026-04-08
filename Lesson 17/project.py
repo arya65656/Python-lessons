9ba93cf410f0848867130d8becc49a4d92a519a9
aarya <chat.the.gpt65@gmail.com>
2026-04-08 11:02:48 +0300

    prject

--- Lesson 17/project.py
+++ Lesson 17/project.py
@@ -5,8 +5,9 @@ print(f"The shopkeeper should return ${change} ")
 
 def change(bill, paid):
     return paid-bill
-bill=2.5
-paid=4.0
+bill=2.50
+paid=4.00
 result=change(bill, paid)
 print(f"The shopkeeper should return: ${result}")
+
 
